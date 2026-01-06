3f8d76c7b0e792af96e8c32027241226234eaf55
kangrae <kangrae0501@gmail.com>
2026-01-06 16:33:28 +0900

    feat: 사용자로부터 어떤 기능을 사용할지 선택받는 기능 구현

--- src/main/java/attendance/controller/Controller.java
+++ src/main/java/attendance/controller/Controller.java
@@ -24,13 +24,35 @@ public class Controller {
 
     public void run() {
         Crews crews = readCrewsFromFile(ATTENDANCES_CSV_DIR);
+
+        String function;
+        while (!"Q".equals(function = readFunction())) {
+            if ("1".equals(function)) {
+            
+            }
+            if ("2".equals(function)) {
+
+            }
+            if ("3".equals(function)) {
+
+            }
+            if ("4".equals(function)) {
+
+            }
+        }
     }
 
-    public Crews readCrewsFromFile(String dir) {
+    private Crews readCrewsFromFile(String dir) {
         return retryUntilValid(() -> {
             List<String> lines = fileReaderService.readFile(dir);
             return InputParser.parseCrews(lines);
         });
+    }
+
+    private String readFunction() {
+        return retryUntilValid(() ->
+                inputView.readFunction()
+        );
     }
 
     private <T> T retryUntilValid(Supplier<T> supplier) {
--- src/main/java/attendance/view/InputView.java
+++ src/main/java/attendance/view/InputView.java
@@ -1,4 +1,12 @@
 package attendance.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+
+    public String readFunction() {
+        System.out.println("오늘은 12월 13일 금요일입니다. 기능을 선택해 주세요.");
+        return Console.readLine();
+    }
+
 }

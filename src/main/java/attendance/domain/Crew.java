d9530cd11ae80e0e468f203ef1869c712c8fe203
kangrae <kangrae0501@gmail.com>
2026-01-06 14:12:27 +0900

    feat: 출석 기능 구현
    - 이미 출석 기록이 있으면 오류를 발생한다.

--- src/main/java/attendance/domain/Crew.java
+++ src/main/java/attendance/domain/Crew.java
@@ -1,16 +1,31 @@
 package attendance.domain;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.Map;
 
 public class Crew {
 
     private final String name;
-    private final Map<LocalDateTime, ArrivalTime> attendance;
+    private final Map<LocalDate, ArrivalTime> attendance;
 
-    private Crew(String name, Map<LocalDateTime, ArrivalTime> attendance) {
+    private Crew(String name, Map<LocalDate, ArrivalTime> attendance) {
         this.name = name;
         this.attendance = attendance;
+    }
+
+    public void attend(LocalDate today, ArrivalTime arrivalTime) {
+        if (attendance.get(today) != null) {
+            throw new IllegalArgumentException("이미 출석을 확인하였습니다. 필요한 경우 수정 기능을 이용해주세요.");
+        }
+        // TODO: 주말 및 공휴일 출석 시도는 오류처리 (이건 Modify에서도 동일하니 메서드로 뽑아내기 또는 Arrival에서 관리하기)
+        attendance.put(today, arrivalTime);
+    }
+
+    public void modify(LocalDate today, ArrivalTime arrivalTime) {
+        if (attendance.get(today) == null) {
+            throw new IllegalArgumentException("출석 기록이 없습니다. 필요한 경우 출석 확인 기능을 이용해주세요.");
+        }
+        attendance.put(today, arrivalTime);
     }
 
 }

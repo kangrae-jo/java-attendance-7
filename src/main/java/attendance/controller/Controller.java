252a7d71afb60a7e5e7e59f35fb8a5777ac6f613
kangrae <kangrae0501@gmail.com>
2026-01-09 13:50:48 +0900

    feat: 컨트롤러 기본 틀 구현

--- src/main/java/attendance/controller/Controller.java
+++ src/main/java/attendance/controller/Controller.java
@@ -7,6 +7,8 @@ import attendance.util.FileReader;
 import attendance.util.InputParser;
 import attendance.view.InputView;
 import attendance.view.OutputView;
+import camp.nextstep.edu.missionutils.DateTimes;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class Controller {
@@ -22,6 +24,25 @@ public class Controller {
     public void run() {
         List<Crew> crews = readCrews(ATTENDANCES_CSV_DIR);
 
+        LocalDateTime now = DateTimes.now();
+        while (true) {
+            String command = inputView.readCommand(now.toLocalDate());
+            if (command.equals("Q")) {
+                return;
+            }
+            if (command.equals("1")) {
+                // 출석 확인
+            }
+            if (command.equals("2")) {
+                // 출석 수정
+            }
+            if (command.equals("3")) {
+                // 출석 기록 확인
+            }
+            if (command.equals("4")) {
+                // 제적 위험자 확인
+            }
+        }
     }
 
     private List<Crew> readCrews(String filePath) {
--- src/main/java/attendance/view/InputView.java
+++ src/main/java/attendance/view/InputView.java
@@ -1,11 +1,23 @@
 package attendance.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.TextStyle;
+import java.util.Locale;
 
 public class InputView {
 
-    public String read() {
+    private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("M월 d일", Locale.KOREAN);
+
+    public String readCommand(LocalDate localDate) {
+        System.out.println("오늘은 " + getLocalDate(localDate) + "입니다. 기능을 선택해 주세요.");
         return Console.readLine();
+    }
+
+    private String getLocalDate(LocalDate localDate) {
+        return localDate.format(dateTimeFormatter) + " "
+                + localDate.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.KOREA);
     }
 
 }
